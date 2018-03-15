ad066dc12b63c5b30d1a3ef1cd32f4520b171419
BakariL <bakarilewis@gmail.com>
2018-03-15 18:09:56 -0400

    first commit

---
+++ FuzzBuzzConsoleApp4.Test/FizzBuzzerTests.cs
@@ -0,0 +1,55 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FuzzBuzzConsoleApp4.Test
+{
+    [TestFixture] //means this will hold test
+    class FizzBuzzerTests
+    {
+        [Test] //first test shuold be something really basic 
+        public void Get_Returns_1_for_1()
+        {
+            var fizzBuzzer = new FizzBuzzer();
+            var result = fizzBuzzer.Get(1); //fizzbuzzer instance that we must create one line above this
+            Assert.That(result, Is.EqualTo("1"));//my result is equal to the string 1
+        }
+
+        [Test]
+        public void Get_Returns_2_for_2()
+        {
+            var fizzBuzzer = new FizzBuzzer();
+            var result = fizzBuzzer.Get(2);
+            Assert.That(result, Is.EqualTo("2"));
+        }
+
+        [Test]
+        public void Get_Returns_Fizz_for_3()
+        {
+            var fizzBuzzer = new FizzBuzzer();
+            var result = fizzBuzzer.Get(3);
+            Assert.That(result, Is.EqualTo("Fizz"));
+        }
+
+
+        [TestCase(1, "1")]
+        [TestCase(2, "2")]
+        [TestCase(3,"Fizz")]//define arguments here
+        [TestCase(6, "Fizz")]
+        [TestCase(5, "Buzz")]
+        [TestCase(10, "Buzz")]
+        [TestCase(15, "FizzBuzz")]
+        [TestCase(30,"FizzBuzz")]
+        public void Get_Returns_Excpected_Value(int input, string expected) //they get passed through here
+        {
+            var fizzBuzzer = new FizzBuzzer();
+            var result = fizzBuzzer.Get(input);
+            Assert.That(result, Is.EqualTo(expected));
+        }
+
+
+    }
+}
---
+++ FuzzBuzzConsoleApp4/FizzBuzzer.cs
@@ -0,0 +1,19 @@
+﻿
+namespace FuzzBuzzConsoleApp4
+{
+    public class FizzBuzzer
+    {
+     
+
+        public string Get(int v)
+        {
+            if (v % 15 == 0)
+                return "FizzBuzz";
+            if (v % 5 == 0)
+                return "Buzz";
+            if (v % 3 == 0)
+                return "Fizz";
+            return v.ToString();
+        }
+    }
+}
